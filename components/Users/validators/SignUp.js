2f1ef1db2a7afd1c6f3275ebd549d466689e5a3b
tdadadavid <davidtofunmidada@gmail.com>
2022-05-25 17:44:15 +0100

    add routes validation

---
+++ components/Users/validators/SignUp.js
@@ -0,0 +1,28 @@
+const userRegistrationSchema = require('../validationSchemas/SignUp');
+
+
+const validateNewUserInputs = async (req, res, next) => {
+
+    try{
+        const { error: err } = await userRegistrationSchema.validate(req.body);
+        if (err){
+            console.log(err);
+            res.status(400).json({
+                status: 400,
+                message: err.message
+            });
+            return;
+        }
+
+        next();
+    }catch (e) {
+        console.log(e);
+        res.status(500).json({
+            status: 500,
+            message: "Oops! an error occurred."
+        });
+    }
+}
+
+
+module.exports = validateNewUserInputs;
